53dfd1d01fdf439473fb689f058aacbce69f65cf
Florian Bellencontre <florian.bellencontre@gmail.com>
2019-11-13 00:03:17 +0100

    bu

--- client-1/index.php
+++ client-1/index.php
@@ -20,7 +20,7 @@
 				DataTables Editor <span>annuaire</span>
 			</h1>
 
-			<?php require($_SERVER['DOCUMENT_ROOT'].'/../../includes/menu.php') ;?>
+			<?php require($_SERVER['DOCUMENT_ROOT'].'/includes/menu.php') ;?>
 
 			<table cellpadding="0" cellspacing="0" border="0" class="display" id="annuaire" width="100%">
 				<thead>
