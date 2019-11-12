dd68c57f5dad759fab118cff8ea73e8675d0941b
Florian Bellencontre <florian.bellencontre@gmail.com>
2019-11-13 00:06:11 +0100

    bu

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,9 @@
 <label for="customer-select">Customer:</label>
 <select name="customer" id="customer-select" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
 	<option value="">--Please choose an option--</option>
-	<option value="../client-1/index.php">Client 1</option>
-	<option value="../client-2/index.php">Client 2</option>
-	<option value="../client-3/index.php">Client 3</option>
+	<option value=$_SERVER['DOCUMENT_ROOT']."/client-1/index.php">Client 1</option>
+	<option value=$_SERVER['DOCUMENT_ROOT']."/client-2/index.php">Client 2</option>
+	<option value=$_SERVER['DOCUMENT_ROOT']."/client-3/index.php">Client 3</option>
 </select>
 
 <br /><br />
