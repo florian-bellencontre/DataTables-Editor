01b4661ebc23be378466b18190ae7597d1c65102
Florian Bellencontre <florian.bellencontre@gmail.com>
2019-11-13 00:57:38 +0100

    bu

--- client-1/index.php
+++ client-1/index.php
@@ -12,7 +12,7 @@
 			<?php 
 				$page_name = dirname(__FILE__);
 				$each_page_name = explode('/', $page_name);
-				$current_directory = echo end($each_page_name);
+				$current_directory = end($each_page_name);
 			?>
 
 		<script type="text/javascript" charset="utf-8" src="https://cdn.datatables.net/v/dt/jqc-1.12.4/moment-2.18.1/dt-1.10.20/b-1.6.1/sl-1.3.1/datatables.min.js"></script>
