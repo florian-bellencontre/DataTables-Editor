8ee03d91b19a63bf1b74ead00a593f59b34b8f1f
Florian Bellencontre <florian.bellencontre@gmail.com>
2019-11-13 02:38:05 +0100

    bu

--- php/table.annuaire.php
+++ php/table.annuaire.php
@@ -1,9 +1,10 @@
 <?php
 
-$page_name = dirname(__FILE__);
-$each_page_name = explode('/', $page_name);
-$current_directory = end($each_page_name);
-print_r($current_directory);
+// $page_name = dirname(__FILE__);
+// $each_page_name = explode('/', $page_name);
+// $current_directory = end($each_page_name);
+// print_r($current_directory);
+
 /*
  * Editor server script for DB table annuaire
  * Created by http://editor.datatables.net/generator
@@ -12,42 +13,44 @@ print_r($current_directory);
 // DataTables PHP library and database connection
 include( "lib/DataTables.php" );
 
+$current_directory = basename(__DIR__);
+
 // Alias Editor classes so they are easy to use
 use
-	DataTables\Editor,
-	DataTables\Editor\Field,
-	DataTables\Editor\Format,
-	DataTables\Editor\Mjoin,
-	DataTables\Editor\Options,
-	DataTables\Editor\Upload,
-	DataTables\Editor\Validate,
-	DataTables\Editor\ValidateOptions;
+        DataTables\Editor,
+        DataTables\Editor\Field,
+        DataTables\Editor\Format,
+        DataTables\Editor\Mjoin,
+        DataTables\Editor\Options,
+        DataTables\Editor\Upload,
+        DataTables\Editor\Validate,
+        DataTables\Editor\ValidateOptions;
 
 // The following statement can be removed after the first run (i.e. the database
 // table has been created). It is a good idea to do this to help improve
 // performance.
-$db->sql( "CREATE TABLE IF NOT EXISTS \"client-1\" (
-	\"id\" serial,
-	\"first_name\" text,
-	\"last_name\" text,
-	\"email\" text,
-	\"phone\" text,
-	\"address\" text,
-	\"postal\" text,
-	\"city\" text,
-	PRIMARY KEY( id )
+$db->sql( "CREATE TABLE IF NOT EXISTS \"$current_directory\" (
+        \"id\" serial,
+        \"first_name\" text,
+        \"last_name\" text,
+        \"email\" text,
+        \"phone\" text,
+        \"address\" text,
+        \"postal\" text,
+        \"city\" text,
+        PRIMARY KEY( id )
 );" );
 
 // Build our Editor instance and process the data coming from _POST
 Editor::inst( $db, $current_directory, 'id' )
-	->fields(
-		Field::inst( 'first_name' ),
-		Field::inst( 'last_name' ),
-		Field::inst( 'email' ),
-		Field::inst( 'phone' ),
-		Field::inst( 'address' ),
-		Field::inst( 'postal' ),
-		Field::inst( 'city' )
-	)
-	->process( $_POST )
-	->json();
+        ->fields(
+                Field::inst( 'first_name' ),
+                Field::inst( 'last_name' ),
+                Field::inst( 'email' ),
+                Field::inst( 'phone' ),
+                Field::inst( 'address' ),
+                Field::inst( 'postal' ),
+                Field::inst( 'city' )
+        )
+        ->process( $_POST )
+        ->json();
