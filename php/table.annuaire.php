1a8b9613b6e0e92af50cfaaeb6acf002d012bfb4
Florian Bellencontre <florian.bellencontre@gmail.com>
2019-11-13 01:35:27 +0100

    bu

--- php/table.annuaire.php
+++ php/table.annuaire.php
@@ -25,7 +25,7 @@ use
 // The following statement can be removed after the first run (i.e. the database
 // table has been created). It is a good idea to do this to help improve
 // performance.
-$db->sql( "CREATE TABLE IF NOT EXISTS \"" . echo $current_directory . "\" (
+$db->sql( "CREATE TABLE IF NOT EXISTS " . $current_directory (
 	\"id\" serial,
 	\"first_name\" text,
 	\"last_name\" text,
