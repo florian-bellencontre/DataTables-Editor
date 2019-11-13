3db2dda5730db15f453e911e68cae2312218e483
Florian Bellencontre <florian.bellencontre@gmail.com>
2019-11-13 02:39:19 +0100

    bu

--- php/table.annuaire.php
+++ php/table.annuaire.php
@@ -13,7 +13,7 @@
 // DataTables PHP library and database connection
 include( "lib/DataTables.php" );
 
-$current_directory = "client2"
+$current_directory = "client2";
 //$current_directory = basename(__DIR__);
 
 // Alias Editor classes so they are easy to use
